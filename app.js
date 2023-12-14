35711947772a465f32e446452b0919331caa32f4
Awhire Elvis <Awhireelvis@gmail.com>
2023-12-14 12:45:15 +0100

    Veiw Engine - Pug

--- app.js
+++ app.js
@@ -6,17 +6,16 @@ const app = express()
 
 // Middleware (serving static assest)
 app.use("/assets", express.static(join(__dirname, "public")))
+app.set("view engine", "pug")
 
 app.get("/", (req, res) => {
     res.status(200).send("<h1>Blog Chef says Hello!</h1>")
 })
 
 // Serving Static Assets and HTML Files
-app.get("/admin/login", (req, res) => {
-    res.sendFile(join(__dirname, "views", "login.html"))
-}).post("/admin/login", (req, res) => {
-    res.send("Handle login here")
-})
+app
+    .get("/admin/login", (req, res) => res.render("login"))
+    .post("/admin/login", (req, res) => res.send("Login API"))
 
 
-app.listen(3000, () => console.log("Blog chef is cooking on port 3000"))    
+app.listen(3000, () => console.log("Blog chef is cooking on port 3000"))     
