d052b17f798cdf66f17ebeccecb6820039261aa4
Awhire Elvis <Awhireelvis@gmail.com>
2023-12-20 04:56:28 +0100

    Integrating / Working with MongoDB Atlas

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,11 +6,12 @@ import getPost from "./get-post";
 import storePost from "./storePost";
 import deletePost from "./deletePost";
 import catchAll from "./catch-all";
+import protectApi from "../../utils/protectApi";
 
 const router = Router() 
 
 router.get("/posts", getPosts)
-router.route("/post/:postId?").get(getPost).post(storePost).delete(deletePost)
+router.route("/post/:postId?").get(getPost).post(protectApi, storePost).delete(protectApi, deletePost)
 router.post("/login", loginUser)
 router.post("/signup", signupUser)
 router.use(catchAll)
